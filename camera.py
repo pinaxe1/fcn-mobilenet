bf5fcd3a29b767152ece4c544466ae5015611351
pinaxe1 <pinaxe@gmail.com>
2019-04-08 19:22:01 -0400

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,20 +1,40 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Apr  7 15:24:01 2019
+Created on Thu Apr  4 21:34:17 2019
+source: https://gist.github.com/cbednarski/8450931
+Intended use:
+To create and automatically annotate datasets. Pixelwize annotation.
+1. Run Application.
+2. Set camera steady with no moving or flickering objects on scene.
+3. Precc "b" button. Camera will take background shot.
+4. Put object to annotate on the scene. 
+5. Press "q" button to take object shots.
+6. The application wil take shot and create two png files. The object and the mask
 
-@author: p
+@author: Pinaxe
 """
 import cv2
 import datetime
 
 cap = cv2.VideoCapture(0)
 cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
+mask=[]
 while(True):
     _, frame = cap.read()
-    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
     cv2.imshow('frame', frame)
     wk = cv2.waitKey(1) & 0xFF
-    if wk == ord('q'): cv2.imwrite(datetime.datetime.now().strftime("%Y%m%d-%H%M%S")+'.png', frame)
-    if wk == ord('x'): break       
+    if wk == ord('b'):
+        # cv2.imwrite(datetime.datetime.now().strftime("%Y%m%d-%H%M%S-Back")+'.png', frame)
+        backdrop=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    if wk == ord('q'):
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)     
+        cv2.absdiff(backdrop,gray,mask)
+        #mask = [0 if a_ > treshold else 255 for a_ in mask]
+        mask[mask<4]=0
+        cv2.imwrite(datetime.datetime.now().strftime("%Y%m%d-%H%M%S-mask")+'.png', mask)
+        #cv2.imwrite(datetime.datetime.now().strftime("%Y%m%d-%H%M%S")+'.png', gray)
+    if wk == ord('x'):
+        break      
+
 cap.release()
-cv2.destroyAllWindows()       
+cv2.destroyAllWindows()      
